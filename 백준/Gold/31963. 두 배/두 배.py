6dcacde976ad47a975cf442b3e8612e0bbbb9f15
Hyunjin <81522656+hyunjin1999@users.noreply.github.com>
2025-03-09 22:59:24 +0900

    [Gold IV] Title: 두 배, Time: 152 ms, Memory: 146588 KB, Score: 33 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.py
+++ 백준/Gold/31963. 두 배/두 배.py
@@ -1,13 +1,10 @@
-import math
-
 n = int(input())
 nums = list(map(int, input().split()))
 current = nums[0]
 ans = 0
 for i in range(1, len(nums)):
-    if nums[i] < nums[i-1]:
-        temp =  math.ceil(math.log2(nums[i-1] / nums[i]))
-        ans += temp
-        nums[i] = nums[i] * (2 ** temp)
+    while nums[i-1] > nums[i]:
+        nums[i] = nums[i] * 2
+        ans += 1
         
 print(ans)
